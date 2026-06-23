9cc9f44b2e35b5985a040859b894f294bb6ff988
jeffdt <jeffdt@gmail.com>
2026-06-23 08:58:56 -0400

    smux 0.4.0

--- Formula/smux.rb
+++ Formula/smux.rb
@@ -1,13 +1,13 @@
 class Smux < Formula
   desc "Fast tmux session picker with pinned sessions and a window tree"
   homepage "https://github.com/jeffdt/smux"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/jeffdt/smux/releases/download/v#{version}/smux-aarch64-apple-darwin"
-      sha256 "d8367b81c34c20fdd5b255df49a3267e3311b3fd8b31ec2fba623303ae3870c4"
+      sha256 "a31f44599a607f45b8d18f05112fa093436d1ace8a1f3b0fcf08fdd82eb20ea6"
     end
   end
 

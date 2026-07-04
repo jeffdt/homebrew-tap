02788189ef9c365ae1c1633d7490933b4c2f7d69
jeffdt <jeffdt@gmail.com>
2026-07-04 01:44:05 -0400

    Bump smux to 0.11.0

--- Formula/smux.rb
+++ Formula/smux.rb
@@ -1,8 +1,8 @@
 class Smux < Formula
   desc "Fast tmux session picker with pinned sessions and a window tree"
   homepage "https://github.com/jeffdt/smux"
-  url "https://github.com/jeffdt/smux/releases/download/v0.10.0/smux-aarch64-apple-darwin"
-  sha256 "344dd2e91ecbba86a59ef15bd4cc6730c8472dc2eb610f1523ffad093add330b"
+  url "https://github.com/jeffdt/smux/releases/download/v0.11.0/smux-aarch64-apple-darwin"
+  sha256 "2207ca8e4e734ff894a0f06c1d709aa5ed811fb5afcb636541cb3a43b88a8010"
   license "MIT"
 
   depends_on arch: :arm64
